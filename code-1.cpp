90ae86d54c8acdbeb074a18c0c166f74daf0874b
walasse07 <walasseyt@gmail.com>
2025-03-26 14:38:24 -0300

    Add files via upload
    Primeiro código

---
+++ code-1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ char nome;
+int idade;
+cout << "Olá, mundo!!";
+
+cout << "\n\tNome: ";
+cin >> nome;
+
+cout << "\n\tIdade: ";
+cin >> idade;
+
+ 
+}
