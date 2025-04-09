6b2bb60cd4bfc3759f38975a59418648ccb9755f
walasse.dev <walasseyt@gmail.com>
2025-04-09 09:55:56 -0300

    Add files via upload

---
+++ percentual.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+float valor1, percentual, resultado;
+
+cout << "Insira um valor: ";
+cin >> valor1;
+
+cout << "Porcentagem desejada: ";
+cin >> percentual;
+
+cout << valor1*percentual/100;
+}
