d9af6cfc7b773f5307b5a62577e2154f819c2ffa
walasse.dev <walasseyt@gmail.com>
2025-04-10 08:16:09 -0300

    Add files via upload

---
+++ c°-em-f°.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+float celsius, fahrenheit, conversao;
+
+cout << "\tTemperatura em celsius: C° ";
+cin >> celsius;
+
+conversao = (9*celsius+160)/5;
+
+cout << " F° " << conversao ;
+
+}
